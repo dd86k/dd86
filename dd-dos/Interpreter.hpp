17176e8a6a28dcee95eb5254fd2652c417704df0
Gian Sass <gian.sass@outlook.de>
2017-01-20 15:07:21 +0100

    Fix compilation

--- dd-dos/Interpreter.hpp
+++ dd-dos/Interpreter.hpp
@@ -38,15 +38,15 @@ private:
         CS, DS, ES, SS,
         // Program Counter (PC)
         IP;
-    bool // From bit 15 to 0:
-        Undefined, // Value Flag
-        Overflow,  // Flag
-        Direction, // Flag
-        Interrupt, // Enable Flag
-        Trap,      // Flag
-        Sign,      // Flag
-        Zero,      // Flag
-        Auxiliary, // Carry Flag
-        Parity,    // Flag
-        Carry,     // Flag
+	bool // From bit 15 to 0:
+		Undefined, // Value Flag
+		Overflow,  // Flag
+		Direction, // Flag
+		Interrupt, // Enable Flag
+		Trap,      // Flag
+		Sign,      // Flag
+		Zero,      // Flag
+		Auxiliary, // Carry Flag
+		Parity,    // Flag
+		Carry;     // Flag
 };
