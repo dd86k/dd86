c587e0eda66050e0fbe0f73e10e44ed3c4066f19
dd <devddstuff@gmail.com>
2017-03-07 17:16:48 -0500

    poshub update

--- src/poshub.d
+++ src/poshub.d
@@ -2,20 +2,22 @@
  * poshub.d : In-house console library.
  */
 
-module poshub;
+module Poshub;
 
 import std.stdio;
-alias sys = core.stdc.stdlib.system;
+private alias sys = core.stdc.stdlib.system;
 
 version (Windows)
 {
-    import core.sys.windows.windows;
-    /// Input and output handles
-    HANDLE hIn, hOut;
+    private import core.sys.windows.windows;
+    private enum ALT_PRESSED =  RIGHT_ALT_PRESSED  | LEFT_ALT_PRESSED;
+    private enum CTRL_PRESSED = RIGHT_CTRL_PRESSED | LEFT_CTRL_PRESSED;
+    /// Necessary handles.
+    private HANDLE hIn, hOut;
 }
 else version (Posix)
 {
-    import core.sys.posix.sys.ioctl;
+    private import core.sys.posix.sys.ioctl;
     //import core.sys.posix.termios;
 }
 
@@ -36,11 +38,24 @@ void InitConsole()
 /// Clear screen
 void Clear()
 {
-    // Lazy alert
     version (Windows)
-        sys ("cls");
+    {
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+        COORD c;
+        GetConsoleScreenBufferInfo(hOut, &csbi);
+        int size = csbi.dwSize.X * csbi.dwSize.Y;
+        DWORD num = 0;
+        if (FillConsoleOutputCharacterA(hOut, ' ', size, c, &num) == 0
+            /*|| // .NET uses this but no idea why.
+            FillConsoleOutputAttribute(hOut, csbi.wAttributes, size, c, &num) == 0*/)
+        { // If that fails, run cls.
+            sys ("cls");
+        }
+    }
     else version (Posix)
+    { //TODO: Clear (Posix)
         sys ("clear");
+    }
     else static assert(0, "Clear: Not implemented");
 }
 
@@ -133,21 +148,22 @@ void Clear()
  * Cursor
  */
 
-/// Set cursor position, 0-based.
-void SetPos(short x, short y)
+/// Set cursor position x and y position respectively from the
+/// top left corner, 0-based.
+void SetPos(int x, int y)
 {
     version (Windows)
-    {
-        COORD c = { x, y };
+    { // 0-based
+        COORD c = { cast(SHORT)x, cast(SHORT)y };
         SetConsoleCursorPosition(hOut, c);
     }
     else version (Posix)
-    { // Works!
+    { // 1-based
         write("\033[", y + 1, ";", x + 1, "H");
     }
 }
 
-@property ushort CursorLeft()
+/*@property ushort CursorLeft()
 {
     version (Windows)
     {
@@ -177,7 +193,7 @@ void SetPos(short x, short y)
 
         return 0;
     }
-}
+}*/
 
 /*
  * Titles
@@ -219,85 +235,48 @@ void SetPos(short x, short y)
     }
 }
 
+/// Get session title
+@property wstring TitleW()
+{
+    version (Windows)
+    {
+        wchar[255] buf;
+        return buf[0..GetConsoleTitleW(&buf[0], MAX_PATH)].idup;
+    }
+    else 
+    {
+        return null;
+    }
+}
+
 /*
  * Input
  */
 
 /// Read a single character.
-char ReadChar(bool echo = false)
-{
-    version (Windows)
-    {
-        INPUT_RECORD ir;
-        ReadConsoleInputA(hOut, &ir, 1, NULL);
-
-        //TODO: while loop
-
-        if (ir.Event.KeyEvent.bKeyDown)
-        {
-            if (echo)
-                write(ir.Event.KeyEvent.uChar.AsciiChar);
-            return ir.Event.KeyEvent.uChar.AsciiChar;
-        }
-
-        return 0;
-    }
-    else version (Posix)
-    {
-        import std.uni : isControl;
-        int t;
-        while (isControl(t = getchar())) {}
-        char c = cast(char)t;
-        if (echo)
-            write(c);
-        return c;
-        /+
-int character;
-struct termios orig_term_attr;
-struct termios new_term_attr;
-
-/* set the terminal to raw mode */
-tcgetattr(fileno(stdin), &orig_term_attr);
-memcpy(&new_term_attr, &orig_term_attr, sizeof(struct termios));
-new_term_attr.c_lflag &= ~(ECHO|ICANON);
-new_term_attr.c_cc[VTIME] = 0;
-new_term_attr.c_cc[VMIN] = 0;
-tcsetattr(fileno(stdin), TCSANOW, &new_term_attr);
-
-/* read a character from the stdin stream without blocking */
-/*   returns EOF (-1) if no character is available */
-character = fgetc(stdin);
-
-/* restore the original terminal attributes */
-tcsetattr(fileno(stdin), TCSANOW, &orig_term_attr);
-
-return character;
-        +/
-    }
-}
-
-/// Read a single character.
 KeyInfo ReadKey(bool echo = false)
 {
     version (Windows)
-    {
-        INPUT_RECORD ir;
-        ReadConsoleInputA(hOut, &ir, 1, NULL);
-
+    { // Sort of is like .NET's ReadKey
         KeyInfo k;
-        
-        k.alt  = ir.KeyEvent.dwControlKeyState & RIGHT_ALT_PRESSED ||
-                    ir.KeyEvent.dwControlKeyState & LEFT_ALT_PRESSED;
-        k.ctrl = ir.KeyEvent.dwControlKeyState & RIGHT_CTRL_PRESSED ||
-                    ir.KeyEvent.dwControlKeyState & LEFT_ALT_PRESSED;
-        k.shift = (ir.KeyEvent.dwControlKeyState & SHIFT_PRESSED) != 0;
-        k.keyChar = ir.KeyEvent.AsciiChar;
-        k.keyCode = ir.KeyEvent.wVirtualKeyCode;
-        k.scanCode = ir.KeyEvent.wVirtualScanCode;
-        k.isKeyDown = ir.KeyEvent.bKeyDown != 0;
 
-        if (k.isKeyDown && echo)
-            write(k.keyChar);
+        INPUT_RECORD ir;
+        DWORD num = 0;
+        if (ReadConsoleInput(hIn, &ir, 1, &num))
+        {
+            if (ir.KeyEvent.bKeyDown && ir.EventType == KEY_EVENT)
+            {
+                DWORD state = ir.KeyEvent.dwControlKeyState;
+                k.alt   = (state & ALT_PRESSED)   != 0;
+                k.ctrl  = (state & CTRL_PRESSED)  != 0;
+                k.shift = (state & SHIFT_PRESSED) != 0;
+                k.keyChar  = ir.KeyEvent.AsciiChar;
+                k.keyCode  = ir.KeyEvent.wVirtualKeyCode;
+                k.scanCode = ir.KeyEvent.wVirtualScanCode;
+ 
+                if (echo) write(k.keyChar);
+            }
+        }
 
         return k;
     }
@@ -309,6 +288,61 @@ KeyInfo ReadKey(bool echo = false)
 
         return k;
     }
+}
+
+RawEvent ReadGlobal()
+{
+    version (Windows)
+    {
+        RawEvent r;
+
+        INPUT_RECORD ir;
+        DWORD num = 0;
+        if (ReadConsoleInput(hIn, &ir, 1, &num))
+        {
+            r.Type = cast(EventType)ir.EventType;
+
+            if (ir.KeyEvent.bKeyDown)
+            {
+                DWORD state = ir.KeyEvent.dwControlKeyState;
+                r.Key.alt   = (state & ALT_PRESSED)   != 0;
+                r.Key.ctrl  = (state & CTRL_PRESSED)  != 0;
+                r.Key.shift = (state & SHIFT_PRESSED) != 0;
+                r.Key.keyChar  = ir.KeyEvent.AsciiChar;
+                r.Key.keyCode  = ir.KeyEvent.wVirtualKeyCode;
+                r.Key.scanCode = ir.KeyEvent.wVirtualScanCode;
+            }
+
+            r.Mouse.Location.X = cast(ushort)ir.MouseEvent.dwMousePosition.X;
+            r.Mouse.Location.Y = cast(ushort)ir.MouseEvent.dwMousePosition.Y;
+            r.Mouse.Buttons = cast(ushort)ir.MouseEvent.dwButtonState;
+            r.Mouse.State = cast(ushort)ir.MouseEvent.dwControlKeyState;
+            r.Mouse.Type = cast(ushort)ir.MouseEvent.dwEventFlags;
+
+            r.Size.Width = ir.WindowBufferSizeEvent.dwSize.X;
+            r.Size.Height = ir.WindowBufferSizeEvent.dwSize.Y;
+        }
+
+        return r;
+    }
+    else version (Posix)
+    {
+        RawEvent r;
+
+        return r;
+    }
+}
+
+enum EventType : ushort {
+    Key = 1, Mouse = 2, Resize = 4
+}
+
+struct RawEvent
+{
+    EventType Type;
+    KeyInfo Key;
+    MouseInfo Mouse;
+    WindowSize Size;
 }
 
 /// Key information structure
@@ -318,9 +352,198 @@ struct KeyInfo
     ///
     char keyChar;
     ///
-    ushort keyCode, scanCode;
+    ushort keyCode;
     ///
-    bool isKeyDown;
+    ushort scanCode;
     ///
-    bool ctrl, alt, shift;
+    bool ctrl;
+    ///
+    bool alt;
+    ///
+    bool shift;
+}
+
+enum MouseButton : ushort {
+    Left = 1, Right = 2, Middle = 4, Mouse4 = 8, Mouse5 = 16
+}
+
+enum MouseState : ushort {
+    RightAlt = 1, LeftAlt = 2, RightCtrl = 4,
+    LeftCtrl = 8, Shift = 0x10, NumLock = 0x20,
+    ScrollLock = 0x40, CapsLock = 0x80, EnhancedKey = 0x100
+}
+
+enum MouseEventType {
+    Moved = 1, DoubleClick = 2, Wheel = 4, HorizontalWheel = 8
+}
+
+struct MouseInfo
+{
+    struct ScreenLocation { ushort X, Y; }
+    ScreenLocation Location;
+    ushort Buttons;
+    ushort State;
+    ushort Type;
+}
+
+struct WindowSize
+{
+    ushort Width, Height;
+}
+
+/// 
+enum Key : ushort {
+    Backspace = 8,
+    Tab = 9,
+    Clear = 12,
+    Enter = 13,
+    Pause = 19,
+    Escape = 27,
+    Spacebar = 32,
+    PageUp = 33,
+    PageDown = 34,
+    End = 35,
+    Home = 36,
+    LeftArrow = 37,
+    UpArrow = 38,
+    RightArrow = 39,
+    DownArrow = 40,
+    Select = 41,
+    Print = 42,
+    Execute = 43,
+    PrintScreen = 44,
+    Insert = 45,
+    Delete = 46,
+    Help = 47,
+    D0 = 48,
+    D1 = 49,
+    D2 = 50,
+    D3 = 51,
+    D4 = 52,
+    D5 = 53,
+    D6 = 54,
+    D7 = 55,
+    D8 = 56,
+    D9 = 57,
+    A = 65,
+    B = 66,
+    C = 67,
+    D = 68,
+    E = 69,
+    F = 70,
+    G = 71,
+    H = 72,
+    I = 73,
+    J = 74,
+    K = 75,
+    L = 76,
+    M = 77,
+    N = 78,
+    O = 79,
+    P = 80,
+    Q = 81,
+    R = 82,
+    S = 83,
+    T = 84,
+    U = 85,
+    V = 86,
+    W = 87,
+    X = 88,
+    Y = 89,
+    Z = 90,
+    LeftMeta = 91,
+    RightMeta = 92,
+    Applications = 93,
+    Sleep = 95,
+    NumPad0 = 96,
+    NumPad1 = 97,
+    NumPad2 = 98,
+    NumPad3 = 99,
+    NumPad4 = 100,
+    NumPad5 = 101,
+    NumPad6 = 102,
+    NumPad7 = 103,
+    NumPad8 = 104,
+    NumPad9 = 105,
+    Multiply = 106,
+    Add = 107,
+    Separator = 108,
+    Subtract = 109,
+    Decimal = 110,
+    Divide = 111,
+    F1 = 112,
+    F2 = 113,
+    F3 = 114,
+    F4 = 115,
+    F5 = 116,
+    F6 = 117,
+    F7 = 118,
+    F8 = 119,
+    F9 = 120,
+    F10 = 121,
+    F11 = 122,
+    F12 = 123,
+    F13 = 124,
+    F14 = 125,
+    F15 = 126,
+    F16 = 127,
+    F17 = 128,
+    F18 = 129,
+    F19 = 130,
+    F20 = 131,
+    F21 = 132,
+    F22 = 133,
+    F23 = 134,
+    F24 = 135,
+    BrowserBack = 166,
+    BrowserForward = 167,
+    BrowserRefresh = 168,
+    BrowserStop = 169,
+    BrowserSearch = 170,
+    BrowserFavorites = 171,
+    BrowserHome = 172,
+    VolumeMute = 173,
+    VolumeDown = 174,
+    VolumeUp = 175,
+    MediaNext = 176,
+    MediaPrevious = 177,
+    MediaStop = 178,
+    MediaPlay = 179,
+    LaunchMail = 180,
+    LaunchMediaSelect = 181,
+    LaunchApp1 = 182,
+    LaunchApp2 = 183,
+    Oem1 = 186,
+    OemPlus = 187,
+    OemComma = 188,
+    OemMinus = 189,
+    OemPeriod = 190,
+    Oem2 = 191,
+    Oem3 = 192,
+    Oem4 = 219,
+    Oem5 = 220,
+    Oem6 = 221,
+    Oem7 = 222,
+    Oem8 = 223,
+    Oem102 = 226,
+    Process = 229,
+    Packet = 231,
+    Attention = 246,
+    CrSel = 247,
+    ExSel = 248,
+    EraseEndOfFile = 249,
+    Play = 250,
+    Zoom = 251,
+    NoName = 252,
+    Pa1 = 253,
+    OemClear = 254
+}
+
+/*
+ * CTRL Handler
+ */
+
+void SetCtrlHandler(void function() f)
+{
+
 }
