9d9ecdadb800e7331bad6505dae5ae55eb1dbaac
dd <devddstuff@gmail.com>
2018-05-04 22:49:40 -0400

    Benchmark fix and improvement

--- bench/Benchmark.d
+++ bench/Benchmark.d
@@ -1,27 +1,28 @@
 module Benchmarks;
 
-import vcpu, std.stdio, vdos, Utilities;
+import vcpu, vcpuutils, std.stdio, vdos, Utilities;
 import std.datetime.stopwatch : StopWatch;
-import core.time : Duration;
-import std.conv : to;
+import core.time, std.conv;
 import core.stdc.string : memcpy;
+
+import unitutils;
 
 unittest
 {
-    writeln("----- Benchmarks");
+    section("Benchmark");
 
-    const uint pos = 10;
-    char[50] c;
-    char* s = cast(char*)"Hello it's me\0";
-    memcpy(cast(void*)&c + pos, s, s.sizeof);
+    sub("String utilities");
+
+    __istr("Hello! This is a test! I love pie.", 10);
 
     StopWatch sw;
 
-    write("MemString(ubyte*, uint) 10'000'000x : ");
+    test("MemString (10'000'000x)");
     sw.start();
-    for (int i; i < 10_000_000; ++i) MemString(cast(void*)c, pos);
+    for (int i; i < 10_000_000; ++i) MemString(10);
     sw.stop();
-    writeln(to!Duration(sw.peek));
+    writefln("%s ms", to!Duration(sw.peek).total!"msecs");
 
+    writeln;
     //sw.reset;
 }
