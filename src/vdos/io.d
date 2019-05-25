c01792819866eee4ebf7ca094c33dcacb1d5c7e7
dd86k <dd@dax.moe>
2019-05-25 13:56:11 -0400

    Start I/O code

--- src/vdos/io.d
+++ src/vdos/io.d
@@ -1,8 +1,9 @@
 /**
- * Virtual I/O handling, mostly I/O port and IN/OUT instructions.
- *
- * All functions have the vio_ prefix, take care of handling the CPU
- * registers.
+ * Virtual I/O handling, mostly I/O port and IN/OUT instructions. Usually
+ * handled by the BIOS. All functions have the vio_ prefix, and take care of
+ * handling the CPU registers. Each port manages their own operations (IO_IN or
+ * IO_OUT set) and may change CPU register values. I/O port may depend on
+ * configured equipment, and only a subset of hardware and ports are emulated.
  */
 module vdos.io;
 
@@ -10,11 +11,50 @@ import vcpu.core : CPU;
 
 extern (C):
 
-
-void vio_send(ushort port) {
+enum : ubyte {
+	IO_IN,	/// Receive (RX)
+	IO_OUT,	/// Transmit (TX)
 	
+	IO_BYTE,	/// Byte data
+	IO_WORD,	/// Word data
+	IO_DWORD,	/// Dword data
 }
 
-void vio_recv(ushort port) {
-	
+/**
+ * Performs an I/O operation (e.g. IN or OUT instruction) to an emulated piece
+ * of hardware. 
+ * Params:
+ *   op = I/O operation
+ *   port = I/O port
+ *   width = Data width (1, 2, or 4 bytes wide)
+ *   data = Tranmission data
+ */
+void io(ubyte op, ushort port, ubyte width, int *data) {
+	switch (port) {
+	//
+	// Monochrome Display Adapter (MDA)
+	//
+	case 0x3B0: break;
+	case 0x3B1: break;
+	case 0x3B2: break;
+	case 0x3B3: break;
+	case 0x3B4: break;
+	case 0x3B5: break;
+	case 0x3B6: break;
+	case 0x3B7: break;
+	case 0x3B8: break;
+	case 0x3BA: break;
+	//
+	// Color Graphic Adapater (CGA)
+	//
+	case 0x3D8: break;
+	case 0x3D9: break;
+	case 0x3DA: break;
+	//
+	// Light Pen (under CGA)
+	//
+	case 0x3DB: break;
+	case 0x3DC: break;
+	default:
+	}
 }
